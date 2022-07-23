df49e0b4fa8d0e4bbd8a78c3df183535fa7a4a30
harrypapas96 <109855242+harrypapas96@users.noreply.github.com>
2022-07-23 22:34:22 +0930

    Version 1.4

--- wp-content/plugins/simply-static/vendor/voku/simple_html_dom/build/generate_docs.php
+++ wp-content/plugins/simply-static/vendor/voku/simple_html_dom/build/generate_docs.php
@@ -13,7 +13,7 @@
 </tr>
 <tr>
 <td bgcolor="#eeeeec" align="center">1</td>
-<td bgcolor="#eeeeec" align="center">0.0001</td>
+<td bgcolor="#eeeeec" align="center">0.0000</td>
 <td bgcolor="#eeeeec" align="right">365072</td>
 <td bgcolor="#eeeeec">{main}(  )</td>
 <td title="/Users/harrypapas/Local Sites/harry-papas-portfolio/app/public/wp-content/plugins/simply-static/vendor/voku/simple_html_dom/build/generate_docs.php" bgcolor="#eeeeec">.../generate_docs.php<b>:</b>0</td>
@@ -34,7 +34,7 @@
 </tr>
 <tr>
 <td bgcolor="#eeeeec" align="center">1</td>
-<td bgcolor="#eeeeec" align="center">0.0001</td>
+<td bgcolor="#eeeeec" align="center">0.0000</td>
 <td bgcolor="#eeeeec" align="right">365072</td>
 <td bgcolor="#eeeeec">{main}(  )</td>
 <td title="/Users/harrypapas/Local Sites/harry-papas-portfolio/app/public/wp-content/plugins/simply-static/vendor/voku/simple_html_dom/build/generate_docs.php" bgcolor="#eeeeec">.../generate_docs.php<b>:</b>0</td>
--- wp-content/themes/perfect-portfolio/single-rara-portfolio.php
+++ wp-content/themes/perfect-portfolio/single-rara-portfolio.php
@@ -16,7 +16,7 @@
 </tr>
 <tr>
 <td bgcolor="#eeeeec" align="center">1</td>
-<td bgcolor="#eeeeec" align="center">0.0001</td>
+<td bgcolor="#eeeeec" align="center">0.0000</td>
 <td bgcolor="#eeeeec" align="right">364768</td>
 <td bgcolor="#eeeeec">{main}(  )</td>
 <td title="/Users/harrypapas/Local Sites/harry-papas-portfolio/app/public/wp-content/themes/perfect-portfolio/single-rara-portfolio.php" bgcolor="#eeeeec">.../single-rara-portfolio.php<b>:</b>0</td>
