0de3945c7b2eae0d01e3935de992b2525debed0a
harrypapas96 <109855242+harrypapas96@users.noreply.github.com>
2022-07-23 22:05:03 +0930

    Version 1.0

---
+++ wp-content/themes/perfect-portfolio/inc/getting-started/tabs/support-panel.php
@@ -0,0 +1,31 @@
+<!-- Support panel -->
+<div id="support-panel" class="panel-left">
+	<div class="toggle-block active">
+		<h3 class="toggle-title">
+<br>
+<font size="1"><table class="xdebug-error xe-uncaught-exception" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Fatal error: Uncaught Error: Call to undefined function esc_html_e() in /Users/harrypapas/Local Sites/harry-papas-portfolio/app/public/wp-content/themes/perfect-portfolio/inc/getting-started/tabs/support-panel.php on line <i>11</i>
+</th></tr>
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Error: Call to undefined function esc_html_e() in /Users/harrypapas/Local Sites/harry-papas-portfolio/app/public/wp-content/themes/perfect-portfolio/inc/getting-started/tabs/support-panel.php on line <i>11</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0000</td>
+<td bgcolor="#eeeeec" align="right">365224</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="/Users/harrypapas/Local Sites/harry-papas-portfolio/app/public/wp-content/themes/perfect-portfolio/inc/getting-started/tabs/support-panel.php" bgcolor="#eeeeec">.../support-panel.php<b>:</b>0</td>
+</tr>
+</table></font>
+</h3>
+</div>
+</div>
